714707373dd9cc5cfcb73bd593da77678a651cbb
gmsvalente <gustavomsvalente@gmail.com>
2022-07-10 12:43:35 -0300

    Create early-init.el file

---
+++ early-init.el
@@ -0,0 +1,62 @@
+;;; early-init.el --- early init file -*- lexical-binding: t; -*-
+
+;; CopyWhat? (©?) 2022 Gustavo Valente
+
+;; Author: Gustavo Valente <gustavomsvalente@gmail.com>
+;; Maintainer: Gustavo Valente <gustavomsvalente@gmail.com>
+;; Version: 0.0.1
+
+;;; Commentary:
+;; this file will run before init.el
+
+;;; Code:
+
+;;; save gc original settings and set new values for startup
+(put 'gc-cons-threshold 'original-value gc-cons-threshold)
+(put 'gc-cons-threshold 'value-during-init most-positive-fixnum)
+
+(put 'gc-cons-percentage 'original-value gc-cons-percentage)
+(put 'gc-cons-percentage 'value-during-init 0.6)
+
+(setq gc-cons-percentage (get 'gc-cons-percentage 'value-during-init))
+(setq gc-cons-threshold (get 'gc-cons-threshold 'value-during-init))
+
+(message "*** Using %d gc-cons-threshold on init ***" gc-cons-threshold)
+(message "*** Using %.2f gc-cons-percentage on init ***" gc-cons-percentage)
+
+
+(setq-default default-frame-alist
+	      '((tool-bar-lines . nil)
+		(width 	 	. 90)
+		(font 		. "Noto Sans Mono")))
+
+(setq file-name-handler-alist nil
+      frame-inhibit-implied-resize t
+      package-enable-at-startup nil)
+
+(defun before-init-fn ()
+  "My startup-function, add it to 'emacs-startup-hook."
+  (message "*** Hello %s ***" (user-login-name))
+  (message "*** Creating .el files ***")
+  (require 'org)
+  (org-babel-load-file ".emacs.d/init.org")
+  (message "*** Emacs initialization in %.3f seconds with %d garbage collections ***"
+	   (float-time (time-subtract after-init-time before-init-time))
+	   gcs-done))
+
+(defun after-init-fn ()
+  "Return gc original values"
+  (message "*** Emacs loaded in %.3f seconds with %d garbage collections ***"
+	   (float-time (time-subtract after-init-time before-init-time))
+	   gcs-done)
+  (setq gc-cons-percentage (get 'gc-cons-percentage 'original-value))
+  (setq gc-cons-threshold (get 'gc-cons-threshold 'original-value))
+  (message "*** Setting gc-cons-threshold  back to %d ***" gc-cons-threshold)
+  (message "*** Setting gc-cons-percentage back to %.2f ***" gc-cons-percentage))
+
+
+(add-hook 'before-init-hook 'before-init-fn)
+(add-hook 'after-init-hook 'after-init-fn)
+
+(provide 'early-init)
+;;; early-init.el ends here
